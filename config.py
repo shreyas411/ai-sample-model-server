6a381e4f142a521ad9e255603458bda2a54440e2
Shreyas Ajith <46870664+shreyas411@users.noreply.github.com>
2025-11-13 22:39:59 +0530

    Implement initial ai model changes
    - Included the summary, keywords, translate in tamil functionalities.
    - Used Qwen Model for this implementation

---
+++ config.py
@@ -0,0 +1,9 @@
+HF_TOKEN = "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"
+BASE_URL = "https://router.huggingface.co/v1"
+MODEL = {
+    "meta_70": "meta-llama/Llama-3.3-70B-Instruct",
+    "meta_8": "meta-llama/Llama-3.1-8B-Instruct",
+    "qwen": "Qwen/Qwen2.5-7B-Instruct",
+    "mistral": "mistralai/Mistral-7B-Instruct-v0.3",
+    "phi": "microsoft/Phi-3-mini-4k-instruct",
+}
---
+++ day8_server.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from openai.types.chat.completion_create_params import ResponseFormat
+from pydantic import BaseModel
+from openai import OpenAI
+import config
+import json
+
+# create server
+app = FastAPI(title="API Server")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# create client
+client = OpenAI(base_url=config.BASE_URL, api_key=config.HF_TOKEN)
+model = config.MODEL["qwen"]
+
+
+# interface for request
+class TextRequest(BaseModel):
+    text: str
+
+
+@app.post("/summary")
+def summary(text: TextRequest):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": f"Summarize the following text: {text.text}",
+            },
+        ],
+        response_format={"type": "json_object"},
+        max_tokens=100,
+    )
+    return json.loads(response.choices[0].message.content)
+
+
+@app.post("/translate")
+def translate(text: TextRequest):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": f"Translate the following text to Tamil: {text.text}",
+            },
+        ],
+        response_format={"type": "json_object"},
+        max_tokens=100,
+    )
+    return json.loads(response.choices[0].message.content)
+
+
+@app.post("/keywords")
+def keywords(text: TextRequest):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": f"Extract keywords from the following text: {text.text}",
+            },
+        ],
+        response_format={"type": "json_object"},
+        max_tokens=100,
+    )
+    return json.loads(response.choices[0].message.content)
